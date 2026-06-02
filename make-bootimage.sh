2d2548c60a95ccfc839e64eb71fd166f3b00daf7
Jami Kettunen <jami.kettunen@protonmail.com>
2026-06-02 13:35:00 +0300

    make-bootimage.sh: Default unified recovery to Google USB VID/PIDs
    Using these which were set on e.g. Volla Phone Plinius stock recovery,
    instead of the fallback zeroed out ones we've been using so far, can
    help with compatiblity on some hosts making UBports installer work more
    broadly with less tinkering.

--- make-bootimage.sh
+++ make-bootimage.sh
@@ -75,6 +75,9 @@ echo "ro.product.name=halium_$deviceinfo_codename" >> prop.default
 if [ "$deviceinfo_use_unified_recovery" = "true" ]; then
     echo "ro.build.version.release=$deviceinfo_halium_version" >> prop.default
     echo "ro.build.version.incremental=ci.ubports.\$(date --utc -d "\$(sed -n 's/ro.build.date=//p' prop.default)" '+%Y%m%d.%H%M%S')" >> prop.default
+    echo "ro.recovery.usb.vid=18D1" >> prop.default
+    echo "ro.recovery.usb.adb.pid=D001" >> prop.default
+    echo "ro.recovery.usb.fastboot.pid=4EE0" >> prop.default
     echo "service.adb.root=1" >> prop.default
 fi
 
